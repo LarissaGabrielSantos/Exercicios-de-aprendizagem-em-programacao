cae064d1fbce12cefd1be47be66e30dec9a38b5c
Larissa <larissagsantos077@gmail.com>
2024-08-29 21:03:39 -0300

    Atualização dos exercícios de Estrutura de Dados

---
+++ Estrutura de Dados/Pilha/Pilha com 10 elementos inteiros.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TAM 10
+
+typedef struct{
+	int vet[TAM];
+	int topo;
+}pilhavet;
+
+void empilha(int valor, pilhavet*p){
+	if(p->topo==TAM-1){
+		printf("\nPilha cheia");
+		exit(0);
+	}
+	p->topo++;
+	p->vet[p->topo]=valor;
+}
+
+int desempilha(pilhavet*p){
+	int aux;
+	if(p->topo<0){
+		printf("\nPilha vazia");
+		exit(1);
+	}
+	aux=p->vet[p->topo];
+	p->topo--;
+	return aux;
+}
+
+int main(void){
+	pilhavet pilha;
+	int valor;
+	pilha.topo=-1;
+	
+	for(int i=0;i<TAM;i++){
+		printf("\nDigite o valor a ser empilhado: ");
+		scanf("%d",&valor);
+		empilha(valor,&pilha);
+	}
+	for(int i=0;i=TAM;i++){
+		printf("\n%d",desempilha(&pilha));
+	}
+	printf("\n");
+	return (0);
+}
