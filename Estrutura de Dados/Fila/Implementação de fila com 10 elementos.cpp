ebd275abc2671774024fe66d849d09b51520ed88
Larissa <larissagsantos077@gmail.com>
2024-09-05 18:28:56 -0300

    Inclusão dos exercícios de fila

---
+++ Estrutura de Dados/Fila/Implementação de fila com 10 elementos.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TAM 10
+
+typedef struct{
+	int r,f;
+	int vet[TAM];
+}filavet;
+
+void enfileirar(int valor, filavet *p){
+	if(p->r == TAM-1){
+		printf("\nFila Cheia!");
+		exit(0);
+	}
+	p->r++;
+	p->vet[p->r] = valor;
+}
+
+int desenfileirar(filavet *p){
+	int aux;
+	if(p->f == TAM){
+		printf("\nFila Vazia!");
+		exit(1);
+	}
+	
+	aux=p->vet[p->f];
+	p->f++;
+	return aux;
+}
+
+int main(void){
+	filavet fila;
+	int valor;
+	fila.r = -1;
+	fila.f = 0;
+	
+	for(int i=0;i<TAM;i++){
+		printf("Digite o valor a ser enfileirado:\n");
+		scanf("%d",&valor);
+		enfileirar(valor,&fila);
+	}
+	
+	for(int i=0;i<TAM;i++){
+		printf("\n%d", desenfileirar(&fila));
+	}
+	
+	
+	return(0);
+}
